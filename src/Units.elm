20c204da44e058226e027a35feffd84ba5be2b6a
Anton Felix Lorenzen <anfelor@posteo.de>
2016-07-30 22:14:32 +0200

    Clean up Units.elm
    This commit removes maps as they break the abstraction, renames onBase to lift, adds  fromBase and restrict conversions with as' to Values of the same unit.

--- src/Units.elm
+++ src/Units.elm
@@ -10,11 +10,10 @@ Create Values of a certain unit, that abstract over a base.
 @docs Value, Unit
 
 # Constructor, conversions and "destructor"
-@docs (~), as', toBase
+@docs (~), as', fromBase, toBase
 
 # Working with the base
-@docs map, map2, map3, map4, map5, map6
-    , onBase, onBase2, onBase3, onBase4, onBase5, onBase6
+@docs lift, lift2, lift3, lift4, lift5, lift6
 
 -}
 
@@ -61,7 +60,7 @@ infix 5 ~
 
   (90 ~ minutes) `as'` seconds
 -}
-as' : Value u1 v1 b -> Unit u2 v2 b -> Value u2 v2 b
+as' : Value u v1 b -> Unit u v2 b -> Value u v2 b
 as' value unit =
   { value = unit.wrap <| value.unit.unwrap value.value
   , unit = unit
@@ -76,152 +75,61 @@ toBase : Value u v b -> b
 toBase value = value.unit.unwrap value.value
 
 
-{-| Map over the base of a Value. 
+{-| Create a Value from a base.
+
+  fromBase seconds 123 : TimeUnit
 -}
-map
-   : (b -> b) 
-  -> Value u1 v1 b
-  -> Value u1 v1 b
-map fn v1 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value))
-  , unit = v1.unit
+fromBase : Unit u v b -> b -> Value u v b
+fromBase unit base =
+  { value = unit.wrap base
+  , unit = unit
   }
-
-
-{-| Map over the bases of Values
-The resulting Value has the Unit of the first argument.
-This is so make partial application more convenient:
-`map2 fn v1` always has the unit of v1, no matter what you apply to it.
--}
-map2 
-   : (b -> b -> b) 
-  -> Value u1 v1 b 
-  -> Value u2 v2 b 
-  -> Value u1 v1 b
-map2 fn v1 v2 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value)) 
-                               (v2.unit.unwrap (v2.value))
-  , unit = v1.unit
-  }
-
-
-{-|-}
-map3
-   : (b -> b -> b -> b) 
-  -> Value u1 v1 b 
-  -> Value u2 v2 b 
-  -> Value u3 v3 b 
-  -> Value u1 v1 b
-map3 fn v1 v2 v3 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value))
-                               (v2.unit.unwrap (v2.value))
-                               (v3.unit.unwrap (v3.value))
-  , unit = v1.unit
-  }
-
-
-{-|-}
-map4 
-   : (b -> b -> b -> b -> b) 
-  -> Value u1 v1 b 
-  -> Value u2 v2 b 
-  -> Value u3 v3 b 
-  -> Value u4 v4 b
-  -> Value u1 v1 b
-map4 fn v1 v2 v3 v4 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value))
-                               (v2.unit.unwrap (v2.value))
-                               (v3.unit.unwrap (v3.value))
-                               (v4.unit.unwrap (v4.value))
-  , unit = v1.unit
-  }
-
-
-{-|-}
-map5 
-   : (b -> b -> b -> b -> b -> b) 
-  -> Value u1 v1 b 
-  -> Value u2 v2 b
-  -> Value u3 v3 b
-  -> Value u4 v4 b
-  -> Value u5 v5 b
-  -> Value u1 v1 b
-map5 fn v1 v2 v3 v4 v5 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value))
-                               (v2.unit.unwrap (v2.value))
-                               (v3.unit.unwrap (v3.value))
-                               (v4.unit.unwrap (v4.value))
-                               (v5.unit.unwrap (v5.value))
-  , unit = v1.unit
-  }
-
-
-{-|-}
-map6 
-   : (b -> b -> b -> b -> b -> b -> b) 
-  -> Value u1 v1 b 
-  -> Value u2 v2 b
-  -> Value u3 v3 b
-  -> Value u4 v4 b
-  -> Value u5 v5 b
-  -> Value u6 v6 b
-  -> Value u1 v1 b
-map6 fn v1 v2 v3 v4 v5 v6 =
-  { value = v1.unit.wrap <| fn (v1.unit.unwrap (v1.value)) 
-                               (v2.unit.unwrap (v2.value))
-                               (v3.unit.unwrap (v3.value))
-                               (v4.unit.unwrap (v4.value))
-                               (v5.unit.unwrap (v5.value))
-                               (v6.unit.unwrap (v6.value))
-  , unit = v1.unit
-  }
-
 
 
 {-| Apply a function to the base of a value.
 -}
-onBase 
+lift 
    : (b -> c)
-  -> Value u1 v1 b
+  -> Value u v1 b
   -> c
-onBase fn v1 =
+lift fn v1 =
   fn (toBase v1)
 
 
 {-| Apply a function to the bases of two values.
 -}
-onBase2 
+lift2 
    : (b -> b -> c)
-  -> Value u1 v1 b
-  -> Value u2 v2 b
+  -> Value u v1 b
+  -> Value u v2 b
   -> c
-onBase2 fn v1 v2 =
+lift2 fn v1 v2 =
   fn (toBase v1)
      (toBase v2)
 
 
 {-|-}
-onBase3 
+lift3 
    : (b -> b -> b -> c)
-  -> Value u1 v1 b
-  -> Value u2 v2 b
-  -> Value u3 v3 b
+  -> Value u v1 b
+  -> Value u v2 b
+  -> Value u v3 b
   -> c
-onBase3 fn v1 v2 v3 =
+lift3 fn v1 v2 v3 =
   fn (toBase v1)
      (toBase v2)
      (toBase v3)
 
 
 {-|-}
-onBase4 
+lift4 
    : (b -> b -> b -> b -> c)
-  -> Value u1 v1 b
-  -> Value u2 v2 b
-  -> Value u3 v3 b
-  -> Value u4 v4 b
+  -> Value u v1 b
+  -> Value u v2 b
+  -> Value u v3 b
+  -> Value u v4 b
   -> c
-onBase4 fn v1 v2 v3 v4 =
+lift4 fn v1 v2 v3 v4 =
   fn (toBase v1)
      (toBase v2)
      (toBase v3)
@@ -229,15 +137,15 @@ onBase4 fn v1 v2 v3 v4 =
 
 
 {-|-}
-onBase5 
+lift5 
    : (b -> b -> b -> b -> b -> c)
-  -> Value u1 v1 b
-  -> Value u2 v2 b
-  -> Value u3 v3 b
-  -> Value u4 v4 b
-  -> Value u5 v5 b
+  -> Value u v1 b
+  -> Value u v2 b
+  -> Value u v3 b
+  -> Value u v4 b
+  -> Value u v5 b
   -> c
-onBase5 fn v1 v2 v3 v4 v5 =
+lift5 fn v1 v2 v3 v4 v5 =
   fn (toBase v1)
      (toBase v2)
      (toBase v3)
@@ -246,16 +154,16 @@ onBase5 fn v1 v2 v3 v4 v5 =
 
 
 {-|-}
-onBase6 
+lift6 
    : (b -> b -> b -> b -> b -> b -> c)
-  -> Value u1 v1 b
-  -> Value u2 v2 b
-  -> Value u3 v3 b
-  -> Value u4 v4 b
-  -> Value u5 v5 b
-  -> Value u6 v6 b
+  -> Value u v1 b
+  -> Value u v2 b
+  -> Value u v3 b
+  -> Value u v4 b
+  -> Value u v5 b
+  -> Value u v6 b
   -> c
-onBase6 fn v1 v2 v3 v4 v5 v6 =
+lift6 fn v1 v2 v3 v4 v5 v6 =
   fn (toBase v1)
      (toBase v2)
      (toBase v3)
